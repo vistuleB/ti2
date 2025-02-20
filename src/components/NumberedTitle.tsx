92776c904ba3c0aa95c0086e2a566a494e31c7dd
MrChaker <mr.chakerw@gmail.com>
2025-02-20 23:59:09 +0100

    Hide counter with value 0

--- src/components/NumberedTitle.tsx
+++ src/components/NumberedTitle.tsx
@@ -1,7 +1,19 @@
-import { ParentProps } from "solid-js";
+import { createEffect, ParentProps } from "solid-js";
 
 const NumberedTitle = (props: ParentProps) => {
-  return <span style={{ color: "blue" }}>{props.children}</span>;
+  let span_ref: HTMLSpanElement | undefined;
+
+  createEffect(() => {
+    if (span_ref) {
+      span_ref.innerText = span_ref.innerText.replace(".0", "");
+    }
+  });
+
+  return (
+    <span ref={span_ref} style={{ color: "blue" }}>
+      {props.children}
+    </span>
+  );
 };
 
 export default NumberedTitle;
