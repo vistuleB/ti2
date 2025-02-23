cbb8b456ce555e1d2b7c37d827ca70fa3474fd83
MrChaker <mr.chakerw@gmail.com>
2025-02-23 21:18:10 +0100

    Fix home page mathjax

--- src/routes/vorlesungsskript.tsx
+++ src/routes/vorlesungsskript.tsx
@@ -1,13 +1,14 @@
 import { MetaProvider, Title } from "@solidjs/meta";
+import Container from "~/components/Container";
 import TOC from "~/components/TOC";
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <MetaProvider>
         <Title title="Ti2 | Table of contents" />
       </MetaProvider>
       <TOC />
-    </>
+    </Container>
   );
 }
